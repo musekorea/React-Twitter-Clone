595c8456b40124a3ca674b37ba771ec623a8866d
musekorea <musekorea@hanmail.net>
2021-11-28 21:55:54 +0800

    dotEnv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from './firebase.js';
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
