f3fbf085dd712da16d041e0f578aba3d511737d9
musekorea <musekorea@hanmail.net>
2021-12-10 00:10:52 +0800

    profile avatar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 import { Link } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar({ loginUser }) {
-  console.log(`네브바`, loginUser.displayName);
   if (loginUser.displayName === null) {
     const name = loginUser.email.split('@')[0];
     loginUser.displayName = name;
   }
-
+  const navigate = useNavigate();
+  const handleLogout = async (e) => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <nav>
       <ul>
@@ -15,8 +25,19 @@ function Navbar({ loginUser }) {
         </li>
         <li>
           <Link to="/profile">{loginUser.displayName}'s Profile</Link>
+          {loginUser.photoURL ? (
+            <img
+              src={loginUser.photoURL}
+              alt=""
+              style={{ width: '20px', borderRadius: '50%' }}
+            />
+          ) : null}
+        </li>
+        <li>
+          <button onClick={handleLogout}>Logout</button>
         </li>
       </ul>
+      <hr></hr>
     </nav>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,23 @@
-import { getAuth, signOut } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { db } from '../firebase.js';
+import { useEffect, useState } from 'react';
+import { db, storage } from '../firebase.js';
 import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import {
+  ref,
+  uploadString,
+  getDownloadURL,
+  deleteObject,
+} from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
 
 function Profile({ loginUser }) {
+  console.log(loginUser);
+  const [isEditPopUp, setIsEditPopUp] = useState(false);
+  const [newNickname, setNewNickname] = useState('');
+  const [newAvatar, setNewAvatar] = useState('');
   const navigate = useNavigate();
-  const handleLogout = async (e) => {
-    const auth = getAuth();
-    try {
-      await signOut(auth);
-      navigate('/');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+
   const getMyTweets = async () => {
     const tweetsRef = collection(db, 'tweets');
     const tweetsQuery = query(
@@ -30,11 +33,91 @@ function Profile({ loginUser }) {
   useEffect(() => {
     getMyTweets();
   }, []);
+
+  const handleEditProfile = (e) => {
+    e.preventDefault();
+    setIsEditPopUp((prev) => !prev);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+    let imageURL;
+    try {
+      if (newAvatar) {
+        const imageRef = ref(storage, `${loginUser.uid}/${uuidv4()}`);
+        const uploadImage = await uploadString(imageRef, newAvatar, 'data_url');
+        imageURL = await getDownloadURL(imageRef);
+      } else {
+        imageURL = null;
+      }
+
+      const photoURL = loginUser.photoURL;
+      if (photoURL && !imageURL) {
+        const deleteRef = ref(storage, `${loginUser.photoURL}`);
+        await deleteObject(deleteRef);
+      }
+
+      await updateProfile(auth.currentUser, {
+        displayName: newNickname,
+        photoURL: imageURL,
+      });
+      navigate('/profile');
+      setNewAvatar(null);
+      setNewNickname('');
+      setIsEditPopUp(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleNickname = (e) => {
+    setNewNickname(e.target.value);
+  };
+
+  const handleAvatar = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e) => {
+      setNewAvatar(reader.result);
+    };
+  };
+
   return (
-    <div>
-      Profile
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <>
+      {!isEditPopUp ? (
+        <button onClick={handleEditProfile}>Edit Profile</button>
+      ) : (
+        <div>
+          <div>
+            <img src={newAvatar} alt="" style={{ width: '50px' }} />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="nickname">Nickname </label>
+            <input
+              id="nickname"
+              type="text"
+              name="displayName"
+              placeholder="Nickname"
+              onChange={handleNickname}
+              value={newNickname}
+            />
+            <p></p>
+            <label htmlFor="avatar">Avatar </label>
+            <input
+              id="avatar"
+              type="file"
+              name="photoURL"
+              placeholder="Avatar"
+              onChange={handleAvatar}
+            />
+            <p></p>
+            <button onClick={handleEditProfile}>Cancel</button>
+            <button>Update</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
